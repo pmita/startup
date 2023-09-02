fb27ffd7c85c5e4c5b0770c8d1708fedd444987b
pmita <panos.mita2@outlook.com>
2023-09-02 22:21:43 +0100

    installing react-hook-form and initial sign in formm

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -6,7 +6,7 @@ interface AuthLayoutProps {
 export default function AuthLayout({children}: AuthLayoutProps) {
   return (
     <section className="min-h-[90dvh] w-full flex justify-center items-stretch">
-      <div className="bg-black flex-1 div-left">
+      <div className="bg-black flex-1 backdrop-banner">
         {/* Image to go here */}
       </div>
       <div className="bg-red flex-1 grid place-content-center z-10">
--- app/(auth)/signin/page.tsx
+++ app/(auth)/signin/page.tsx
@@ -1,6 +1,45 @@
+"use client" 
 
-export default async function SignInPage(){
+import { SubmitHandler, useForm } from 'react-hook-form';
+import './style.css';
+// FONTS
+import { poppins, roboto } from '@/app/font';
+
+interface IFormInput {
+  email: string;
+  password: string;
+}
+
+export default function SignInPage(){
+  const { register, handleSubmit } = useForm<IFormInput>();
+
+  // EVENTS
+  const onSubmit: SubmitHandler<IFormInput> = (data) => console.log(data);
   return (
-    <h1>Hello from SignIn Page</h1>
+    <form onSubmit={handleSubmit(onSubmit)} className="w-xl sm:w-lg flex flex-col justify-center items-stretch gap-5">
+      <h1 className="w-full">Welcome, please sign in!</h1>
+      <input
+        className="text-black"
+        placeholder='Email'
+        {...register("email", {
+          required: true,
+          maxLength: 80,
+          minLength: 5,
+          pattern: /^\S+@\S+$/i,
+        })}
+      />
+
+      <input
+        placeholder="Password"
+        {...register("password", {
+          required: true,
+          maxLength: 25,
+          minLength: 5,
+          pattern: /^\S+@\S+$/i,
+        })}
+      />
+
+      <button className="button" type="submit">Log In</button>
+    </form>
   );
 }
