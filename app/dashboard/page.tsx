14125475b3c0c796cab6cf1f157d1ddd0a6a3c78
pmita <panos.mita2@outlook.com>
2023-09-28 22:09:09 +0100

    AuthCheck component upon accessing dashboard page

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,25 +1,32 @@
 "use client" 
 
-import { useEffect } from 'react';
-import { useRouter } from 'next/router';
 // HOOKS
 import { useAuthContext } from '@/hooks/useAuthContext';
+// COMPONENTS
+import AuthCheck from '@/components/AuthCheck';
+import Avatar from '@/components/Avatar';
+import { SignInButton } from '@/components/Buttons';
 
 export default function SignInPage(){
   // HOOKS
-  const router = useRouter();
   const { user } = useAuthContext();
 
-  // USE EFFECTS
-  useEffect(() => {
-    if (!user) {
-      router.push('/');
-    }
-  }, [router, user]);
-
   return (
-    <section>
-      <h1>Welcome to User Profile</h1>
+    <section className="grid place-items-center">
+      <AuthCheck fallback={(
+        <>
+          <h1>You are not signed it ... this page means nothing to you</h1>
+          <SignInButton />
+        </>
+      )}>
+        <h1>Welcome to you dashboard, {user?.displayName}</h1>
+        <Avatar
+          src={user?.photoURL}
+          width={100}
+          height={100}
+          altText={user?.displayName ?? 'John Doe'}
+        />
+      </AuthCheck>
     </section>
   );
 }
