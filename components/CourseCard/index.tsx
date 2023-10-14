82655afd3ab11ceb39fe8d71ad7076825fb1a0b0
pmita <panos.mita2@outlook.com>
2023-10-15 00:59:22 +0100

    header component added and major UI changes

--- components/CourseCard/index.tsx
+++ components/CourseCard/index.tsx
@@ -18,7 +18,7 @@ export default async function CourseCard({
 }: CourseCardProps) {
   return (
   <div className="full columnTopCenter gap-4 shadow-xl rounded-[12px]">
-    <div className="full cardImageBox bg-primary-blue rounded-[12px]">
+    <div className="full cardImageBox bg-primary-blue rounded-t-[12px]">
       <Link href="/courses/[slug]" as={`/courses/${title}`}>
         <ImageWithFallback
           src={'/images/hacker.png'}
@@ -35,17 +35,18 @@ export default async function CourseCard({
         className="absolute right-[5%] top-[5%] bg-primary-black px-2 py-1 text-center font-bold text-primary-white">
         New
       </span>
-    <div className="detailsBox absolute columnCenterLeft gap-2 w-[90%] translate-x-[-0%] translate-y-[10%] transition-all duration-[0.3s] ease-[ease-in-out] overflow-hidden px-4 py-2 rounded-xl left-[5%] bottom-[10%] h-2/5 bg-primary-white p-4">
-      <div className="flex justify-start items-center gap-2">
-        {hastags && hastags.map((hastag) => <Tag key={hastag} tag={hastag} /> )}
+      
+      <div className="detailsBox absolute columnCenterLeft gap-2 w-[90%] translate-x-[-0%] translate-y-[10%] transition-all duration-[0.3s] ease-[ease-in-out] overflow-hidden px-4 py-2 rounded-b-[12px] left-[5%] bottom-[10%] h-2/5 bg-primary-white p-4">
+        <div className="flex justify-start items-center gap-2">
+          {hastags && hastags.map((hastag) => <Tag key={hastag} tag={hastag} /> )}
+        </div>
+        <h5 className="mb-2 text-xl tracking-wide text-primary-black font-poppins font-bold">
+          {title}
+        </h5>
+        <p className="mb-2 text-base text-primary-black font-roboto">
+          {description}
+        </p>
       </div>
-      <h5 className="mb-2 text-xl tracking-wide text-primary-black font-poppins font-bold">
-        {title}
-      </h5>
-      <p className="mb-2 text-base text-primary-black font-roboto">
-        {description}
-      </p>
-    </div>
     </div>
 
   </div>
