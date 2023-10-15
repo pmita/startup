3346cf888964bb53ba1c630ed176a899dedaaf18
pmita <panos.mita2@outlook.com>
2023-10-15 20:23:08 +0100

    adding features section under homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 // COMPONENTS
+import FeaturesBanner from "@/components/Banners/FeaturesBanner";
 import LandingBanner from "@/components/Banners/LandingBanner";
 
 export default async function Home() {
   return (
     <div>
-      <LandingBanner />
+      {/* <LandingBanner /> */}
+      <FeaturesBanner />
     </div>
   )
 }
