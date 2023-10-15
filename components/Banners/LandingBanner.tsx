a3c8a73fded34a3711e045680459e9394aa7f5c0
pmita <panos.mita2@outlook.com>
2023-10-15 13:35:28 +0100

    header component in the homepage

--- components/Banners/LandingBanner.tsx
+++ components/Banners/LandingBanner.tsx
@@ -3,6 +3,7 @@
 import Link from 'next/link'
 // COMPONENTS
 import { CylinderSVG } from '../SVGs';
+import Header from '../Header';
 
 
 export const LandingBanner = () => {
@@ -24,12 +25,11 @@ export const LandingBanner = () => {
         {/* ----- CTA Banner ----- */}
         <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
           <div className="text-center">
-            <h1 className="text-4xl font-bold font-poppins tracking-tight sm:text-6xl">
-              Data to enrich your online business
-            </h1>
-            <p className="mt-6 text-lg font-roboto leading-8 text-gray-600">
-              Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat fugiat aliqua.
-            </p>
+            <Header 
+              heading="Data to enrich your online business"
+              subHeading="Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat fugiat aliqua."
+              className="flex flex-col justify-center items-center gap-6"            
+            />
             <div className="mt-10 flex items-center justify-center gap-x-6">
               <button className="button">
                 <Link href={'/pro'}>
