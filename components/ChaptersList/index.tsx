3aae9055d8c5813d895f847e6e9d8c9195600e51
pmita <panos.mita2@outlook.com>
2023-10-19 23:13:48 +0100

    Authcheck component included

--- components/ChaptersList/index.tsx
+++ components/ChaptersList/index.tsx
@@ -1,4 +1,5 @@
 // COMPONENTS
+import AuthCheck from "../AuthCheck"
 import Tag from "../CourseCard/Tag"
 // LIBRARIES
 import { Courses } from "@/.contentlayer/generated"
@@ -7,13 +8,23 @@ type ChapterListProps = {
   chapters: Courses[]
 }
 
-
 export default async function ChaptersList({ chapters }: ChapterListProps) {
   return (
-    <aside className="flex-[1_1_200px] self-stretch w-full order-2 sm:order-1">
+    <aside className="flex-[1_1_200px] self-stretch flex flex-col flex-start items-between gap-2 w-full order-2 sm:order-1">
       {chapters.map((course, index) => (
-        <div className="flex justify-between items-center" key={index}>
-          <h3>{course.title}</h3>
+        <div className="flex justify-between items-center p-2" key={index}>
+          <span className="flex justify-start items-center gap-2">
+            <AuthCheck
+              fallback={(
+                <div className="w-[20px] h-[20px] rounded-[50%] bg-primary-black opacity-75"></div>
+              )}
+            >
+              <div className="w-[20px] h-[20px] rounded-[50%] bg-primary-black opacity-75"></div>
+              <div className="w-[20px] h-[20px] rounded-[50%] bg-primary-green"></div>
+
+            </AuthCheck>
+            <h3>{course.title}</h3>
+          </span>
           <Tag tag={course?.video_length || ''} />
         </div>
       ))}
