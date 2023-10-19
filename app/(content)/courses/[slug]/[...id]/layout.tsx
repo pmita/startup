29100583efd1c696ada13136c39044a63caf2899
pmita <panos.mita2@outlook.com>
2023-10-19 17:55:05 +0100

    getting flexbox working

--- app/(content)/courses/[slug]/[...id]/layout.tsx
+++ app/(content)/courses/[slug]/[...id]/layout.tsx
@@ -9,18 +9,18 @@ interface CourseChapterLayoutProps {
 }
 
 export default function CourseChapterLayout({ children, params }: CourseChapterLayoutProps) {
-  const courses = allCourses.filter((course) => course.slugAsParams.split("/")[0] === params.slug && course?._raw.sourceFileName !== 'index.mdx')
+  const chapters = allCourses.filter((course) => course.slugAsParams.split("/")[0] === params.slug && course?._raw.sourceFileName !== 'index.mdx')
 
   return (
-    <section className="flex flex-row justify-center items-stretch gap-16 w-full h-[90vh]">
-      <aside className="grow-1 self-stretch w-full">
-        {courses.map((course, index) => (
+    <section className="flex flex-wrap flex-row justify-center items-stretch gap-16 w-full h-[90vh]">
+      <aside className="flex-[1_1_200px] self-stretch w-full order-2 sm:order-1">
+        {chapters.map((course, index) => (
           <li key={index}>
             <h1>{course.title}</h1>
           </li>
         ))}
       </aside>
-      <section className="grow-4 self-stretch w-full bg-red-400">
+      <section className="flex-[4_1_550px] self-stretch w-full bg-red-400 order-1 sm:order-2">
         {children}
       </section>
     </section>
