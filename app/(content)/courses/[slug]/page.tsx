15ee4c32dd8a8a3b6b5138cd389dd17738eaa3a8
pmita <panos.mita2@outlook.com>
2023-11-01 21:35:59 +0000

    minor tweaks in the course chapters grid

--- app/(content)/courses/[slug]/page.tsx
+++ app/(content)/courses/[slug]/page.tsx
@@ -70,9 +70,9 @@ export default async function LessonPage({ params }: CoursePageProps) {
           />
         }
       />
-      <section className="grid grid-cols-[repeat(auto-fill,minmax(240px,300px))] auto-rows-[150px] gap-8 mx-0 my-4 p-4 justify-center">
+      <section className="grid grid-cols-[repeat(auto-fit,minmax(240px,240px))] auto-rows-[150px] gap-8 mx-0 my-4 p-4 justify-center">
         {chapters.map((chapter) => (
-          <div key={chapter.weight} className="w-full border-[5px] border-solid border-primary-black hover:border-primary-green columnCenterLeft gap-2 rounded-[12px] bg-primary-white p-4 py-2">
+          <div key={chapter.weight} className="w-full border-[6px] border-solid border-primary-black hover:border-primary-green flex flex-col justify-center items-center rounded-[12px] bg-primary-white">
             <Link href={`/courses/${chapter.slugAsParams}`}>
               <InfoCard
                 title={chapter?.title}
