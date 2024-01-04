d4fa3ef3247e0625ceda859b52b92a41f87a8fc0
pmita <panos.mita2@outlook.com>
2024-01-04 16:01:49 +0000

    chore: remove unused svg

--- components/InfoCard/index.tsx
+++ components/InfoCard/index.tsx
@@ -1,6 +1,5 @@
 // COMPONENTS
 import Tag from "../CourseCard/Tag"
-import { NextSvg } from "../SVGs";
 // UTILS
 import { cn } from "@/utils/helpers";
 
@@ -21,22 +20,17 @@ export default async function InfoCard({
 }: InfoCardProps) {
   return (
     <div className={cn(
-        "w-full flex flex-col justify-start item-start gap-2.5 p-2.5",
+        "w-full flex flex-col justify-start item-start",
         className
       )}
     >
-
-      {icon && (
-        <NextSvg width={48} height={48} fill={'#000'} />
-        )}
-      
       {hastags && hastags.length && (
-        <div className="flex justify-start items-center gap-1">
+        <div className="flex justify-start items-center gap-2">
           {hastags && hastags.map((hastag) => (
             <Tag 
               key={hastag} 
               tag={hastag} 
-              className="bg-primary-black rounded-[6px]"
+              className="bg-primary-black"
             /> 
           ))}
         </div>
