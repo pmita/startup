2ffe4915d4950ad9ced34fc3f8b4b1679e74a5c6
pmita <panos.mita2@outlook.com>
2024-01-17 22:32:34 +0000

    fix(video-player): handling edge case of non free videos

--- components/VideoPlayer/index.tsx
+++ components/VideoPlayer/index.tsx
@@ -1,14 +1,13 @@
 "use client"
 
-import { useCallback, useEffect, useRef, useState } from "react"
+import { useEffect, useRef, useState } from "react"
 import Vimeo from '@vimeo/player';
-import { Options } from "@vimeo/player";
 // COMPONENTS
 import { buttonVariants } from "../ui/Button";
-import { ToggleProgressButton } from "../Buttons";
 // UTILS
 import { cn } from "@/utils/helpers";
 import { useIsSubscriptionValid } from "@/hooks/useIsSubscriptionValid";
+import Link from "next/link";
 
 interface VimeoPlayerProps {
     videoId: number | undefined
@@ -17,32 +16,34 @@ interface VimeoPlayerProps {
 }
 
 export const VideoPlayer: React.FC<VimeoPlayerProps> = ({videoId, isFree, onVideoEnded}) => {
-  const playerRef = useRef<Vimeo | null>(null);
+  const playerRef = useRef<any | null>(null);
   const [isPlaying, setIsPlaying] = useState(false);
-  const [shouldAutoPlay, setShouldAutoPlay] = useState(false);
   const canAccess = useIsSubscriptionValid();
 
   useEffect(() => {
-    if (!playerRef.current && videoId) {
-      playerRef.current = new Vimeo('video-player', {
-        id: videoId as number,
-        controls: true,
-        quality: 'auto',
-        title: false,
-        byline: false,
-      })
+    if (!playerRef.current) {
+      if (isFree || canAccess) {
+        playerRef.current = new Vimeo('video-player', {
+          id: videoId,
+          controls: true,
+          quality: 'auto',
+          title: false,
+          byline: false,
+        });
 
-      playerRef.current.on('ended', () => {
-        onVideoEnded && onVideoEnded()
-        setIsPlaying(false)
-      })
-    }
-    return () => {
-      if (playerRef.current) {
-        playerRef.current.off('ended')
+        playerRef.current.on('ended', () => {
+          onVideoEnded && onVideoEnded();
+          setIsPlaying(false);
+        });
       }
     }
-  }, [onVideoEnded, videoId])
+
+    return () => {
+      if (playerRef.current) {
+        playerRef.current.off('ended');
+      }
+    };
+  }, [onVideoEnded, videoId, isFree, canAccess]);
 
   useEffect(() => {
     if (playerRef.current) {
@@ -54,23 +55,26 @@ export const VideoPlayer: React.FC<VimeoPlayerProps> = ({videoId, isFree, onVide
     }
   }, [isPlaying]);
 
-  
-  // EVENTS
-  const handleAutoPlay = useCallback(() => {
-    setShouldAutoPlay(!shouldAutoPlay);
-  }, [shouldAutoPlay]);
-  
   if (!videoId) return null;
 
   return (
     <>
     {videoId && (isFree || canAccess) ? (
-      <div className="aspect-video w-full relative bg-primary bg-opacity-50">
+      <div className="aspect-video w-full relative bg-secondary bg-opacity-50">
         <div data-vimeo-logo="false" id="video-player" className="absolute top-0 left-0 w-full h-full" />
       </div>
     ) : (
-      <div className="aspect-video w-full relative bg-primary bg-opacity-50">
-        <h1>Test</h1>
+      <div className="aspect-video w-full relative bg-secondary grid place-content-center gap-10">
+        <h2 className="font-bold text-neutral">You can not view this video, upgrade now</h2>
+        <Link
+          href="/pro"
+          className={cn(buttonVariants({
+            variant: 'primary',
+            className: 'mt-4'
+          }))}
+        >
+          Upgrade to PRO
+        </Link>
       </div>
     )}
     </>
