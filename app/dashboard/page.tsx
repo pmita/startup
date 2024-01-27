5e2342bb252ecd6d4326e6b17a69cee0dc0463b0
pmita <panos.mita2@outlook.com>
2024-01-27 19:20:00 +0000

    refactor: introducing authCheck component at a layout level

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -2,16 +2,15 @@
 
 // NEXT
 import { type Metadata } from 'next';
-import Link from 'next/link';
 // HOOKS
 import { useAuthState } from '@/hooks/useAuthState';
 // COMPONENTS
-import AuthCheck from '@/components/AuthCheck';
 import Avatar from '@/components/Avatar';
 import { buttonVariants, Button } from '@/components/ui/Button';
+// HOOKS
+import { useSignOut } from '@/hooks/useSignOut';
 // UTILS
 import { cn } from '@/utils/helpers';
-import { useSignOut } from '@/hooks/useSignOut';
 
 export const MetaData: Metadata = {
   metadataBase: new URL(process.env.NEXT_PUBLIC_BASE_URL || 'https://example.com'),
@@ -26,29 +25,20 @@ export default function DashboardPage(){
 
   return (
     <section className="grid place-items-center gap-2">
-      <AuthCheck fallback={(
-        <>
-          <h1>You are not signed it ... this page means nothing to you</h1>
-          <Link href="/signin" className={cn(buttonVariants({ variant: "primary" }))}>
-            Sign In
-          </Link>
-        </>
-      )}>
-        <h1>Welcome to you dashboard, {user?.displayName}</h1>
-        <Avatar
-          src={'/${user?.photoURL}'}
-          width={250}
-          height={250}
-          altText={user?.displayName ?? 'John Doe'}
-        />
-        <Button
-          className={cn(buttonVariants({ variant: "primary" }))}
-          onClick={signOut}
-          disabled={isLoading}
-        >
-          {isLoading ? 'Loading...' : 'Sign Out'}
-        </Button>
-      </AuthCheck>
+      <h1>Welcome to you dashboard, {user?.displayName}</h1>
+      <Avatar
+        src={'/${user?.photoURL}'}
+        width={250}
+        height={250}
+        altText={user?.displayName ?? 'John Doe'}
+      />
+      <Button
+        className={cn(buttonVariants({ variant: "primary" }))}
+        onClick={signOut}
+        disabled={isLoading}
+      >
+        {isLoading ? 'Loading...' : 'Sign Out'}
+      </Button>
     </section>
   );
 }
