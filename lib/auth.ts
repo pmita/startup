6306d3796f5b6f031d2c0799f4947bd3b38708ae
pmita <panos.mita2@outlook.com>
2024-03-10 21:41:31 +0000

    feat(cookie-authentication): adding, removing, and setting js cookies

--- lib/auth.ts
+++ lib/auth.ts
@@ -2,6 +2,8 @@
 import { firebaseAuth, firestore } from "@/utils/firebase-admin";
 // STRIPE
 import { stripe } from "@/utils/stripe";
+// PACKAGES
+import Cookies from 'js-cookie';
 // TYPES
 import Stripe from "stripe";
 
@@ -44,3 +46,22 @@ export async function getOrCreateCustomer(uid: string, params?: Stripe.CustomerC
     return await stripe.customers.retrieve(stripeCustomerId);
   }
 }
+
+export function getAuthToken(): string | undefined {
+  return Cookies.get('__session');
+}
+
+export function removeAuthToken(): void {
+  return Cookies.remove('__session');
+}
+
+export function setAuthToken(token: string): string | undefined {
+  const expiresIn = 60 * 60 * 24 * 5 * 1000; // expire cookies after 5 days
+
+  return Cookies.set('__session', token, {
+    expires: expiresIn,
+    secure: true,
+    http: true,
+  });
+}
+
