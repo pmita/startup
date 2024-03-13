0897b7539d4c9a2a54828fac556da84ed0505d9d
pmita <panos.mita2@outlook.com>
2024-03-13 22:23:06 +0000

    feat(cookie-authentication): route guarding dashboard pages

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,35 +1,28 @@
-"use client"
-
 // NEXT
-import Link from "next/link";
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
 // COMPONENTS
-import AuthCheck from "@/components/AuthCheck";
 import DashboardList from "@/components/DashboardList";
-import { buttonVariants } from "@/components/ui/Button";
-// UTILS
-import { cn } from "@/utils/helpers";
 // CONFIG
 import { dashboardConfig } from "@/config/dashboard";
 
 
 export default function DashboardLayout({ children }: { children: React.ReactNode }) {
+  // SERVER LAND
+  const nextCookies = cookies();
+  const authToken = nextCookies.get('__session');
+
+  if (!authToken) {
+    redirect('/signin');
+  }
   return (
     <div className="flex flex-wrap flex-row justify-center items-stretch w-full p-5 pb-0">
-      <AuthCheck fallback={(
-        <div className="grid place-items-center gap-10">
-        <h1>You are not signed it ... this page means nothing to you</h1>
-        <Link href="/signin" className={cn(buttonVariants({ variant: "primary" }))}>
-          Sign In
-        </Link>
-      </div>
-      )}>
         <aside className="flex-[1_1_300px] self-stretch flex flex-col flex-start items-between gap-2 w-full h-[auto] scroll-auto p-5">
           <DashboardList items={dashboardConfig.sideNav} />
         </aside>
         <section className="p-5 flex-[4_1_670px] self-stretch w-full order-1 lg:order-2">
           {children}
         </section>
-      </AuthCheck>
       
     </div>
   )
