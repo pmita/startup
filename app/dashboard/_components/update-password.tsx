ac28993aaf5edf71ef3826b3c7925746afdaf91b
pmita <panos.mita2@outlook.com>
2024-03-21 23:37:59 +0000

    feat(update-password): implementing modal component

--- app/dashboard/_components/update-password.tsx
+++ app/dashboard/_components/update-password.tsx
@@ -1,5 +1,7 @@
 "use client" 
 
+// REACT
+import { useState } from "react";
 import { 
   Card, 
   CardContent, 
@@ -7,12 +9,23 @@ import {
   CardHeader, 
   CardTitle 
 } from "@/components/ui/card";
+import {
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogFooter
+} from "@/components/ui/dialog";
 import { Button, buttonVariants } from "@/components/ui/button";
 // UTILS
 import { cn } from "@/utils/helpers";
+import { set } from "date-fns";
 
 export function UpdatePassword() {
+  // STATE && VARIABLES
+  const [isOpen, setIsOpen] = useState(false);
   return (
+    <>
     <Card>
       <CardHeader>
         <CardTitle className="font-semibold">Password</CardTitle>
@@ -21,18 +34,53 @@ export function UpdatePassword() {
         </CardDescription>
       </CardHeader>
       <CardContent>
-        <UpdatePasswordButton />
+        <UpdatePasswordButton onClick={() => setIsOpen(true)} />
       </CardContent>
     </Card>
+    
+    {isOpen && (
+      <UpdatePasswordDialog 
+        onClick={() => setIsOpen(false)} 
+      />
+    )}
+    </>
   )
 }
 
-export function UpdatePasswordButton() {
+export function UpdatePasswordButton({ onClick }: { onClick: () => void }) {
   return (
     <Button
       className={cn(buttonVariants({ variant: "secondary" }))}
+      onClick={onClick}
     >
       Update Password
     </Button>
+  )
+}
+
+export function UpdatePasswordDialog({ onClick }: { onClick: () => void }) {
+  return (
+    <DialogContent>
+      <DialogHeader>
+        <DialogTitle>Update Password</DialogTitle>
+        <DialogDescription>
+          Update your password. This will only affect email and password login
+        </DialogDescription>
+      </DialogHeader>
+      <DialogFooter>
+        <Button
+          className={cn(buttonVariants({ variant: "secondaryOutlined" }))}
+          onClick={onClick}
+        >
+          Cancel
+        </Button>
+        <Button
+          className={cn(buttonVariants({ variant: "secondary" }))}
+          onClick={onClick}
+        >
+          Update Password
+        </Button>
+      </DialogFooter>
+    </DialogContent>
   )
 }
