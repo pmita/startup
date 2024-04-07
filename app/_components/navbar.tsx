0ff37dc0f9d7ea38a841fb82d88737c577361f2e
pmita <panos.mita2@outlook.com>
2024-04-07 20:04:43 +0100

    style(navbar): fixing spacing issues for mobile views

--- app/_components/navbar.tsx
+++ app/_components/navbar.tsx
@@ -4,17 +4,17 @@ import { ShouldShowProLink, ShouldShowDashboardLink } from './authed-options';
 
 export async function Navbar() {
   return (
-    <nav className="min-h-10 w-screen p-10 md:p-8 flex justify-between items-center bg-white">
+    <nav className="min-h-10 w-screen p-4 sm:p-10 md:p-8 flex justify-between items-center bg-white">
       <div className="flex justify-center items-center">
         <Link href={"/"}>
           <h2 
-            className="text-2xl p-4 font-poppins font-bold"
+            className="text-2xl font-poppins font-bold"
           >
             Codaro<span className="text-primary">.io</span>
           </h2>
         </Link>
       </div>
-      <ul className="flex-1 flex justify-end items-center gap-10 content-between">
+      <ul className="flex-1 flex justify-end items-center gap-5 content-between">
         <ShouldShowProLink />
         <li className="hover:scale-105 hover:opacity-75 font-semibold">
           <Link href={"/blog"}>
