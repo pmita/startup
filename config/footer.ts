da726f2e7d468243876fc93d93800b08cc0b4856
pmita <panos.mita2@outlook.com>
2024-04-08 22:48:40 +0100

    feat(footer): adding dynamic footer

---
+++ config/footer.ts
@@ -0,0 +1,22 @@
+export const footerLinks = [
+  {
+    id: 1,
+    title: 'Blog',
+    route: '/blog'
+  },
+  {
+    id: 2,
+    title: 'Courses',
+    route: '/courses'
+  },
+  {
+    id: 3,
+    title: 'Privacy Policy',
+    route: '/privacy-policy'
+  },
+  {
+    id: 4,
+    title: 'Terms and Conditions',
+    route: '/terms-and-conditions'
+  },
+]
