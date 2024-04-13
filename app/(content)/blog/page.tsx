711a0997882b8204ac6be041ad154c9608e1d0fd
pmita <panos.mita2@outlook.com>
2024-04-13 20:40:28 +0100

    refactor(banners): implementing composable banners on /blog page

--- app/(content)/blog/page.tsx
+++ app/(content)/blog/page.tsx
@@ -2,7 +2,7 @@
 import { type Metadata } from "next";
 import Link from "next/link";
 // COMPONENTS
-import { Banner, bannerVariants } from "@/components/ui/banner";
+import { Banner, BannerHeader, bannerVariants } from "@/components/ui/bannerz";
 import { Header } from "@/components/ui/header";
 import { Title, titleVariants } from '@/components/ui/title';
 import { Description, descriptionVariants } from "@/components/ui/description";
@@ -28,46 +28,33 @@ export default async function BlogPage() {
 
   return (
     <>
-      <Banner
-        className={cn(bannerVariants({
-          size: "default",
-          className: "rounded-[12px] min-h-[505px] w-full bg-primary-black text-primary-white flex flex-col justify-center items-start gap-10 p-10"
-        }))}
-        bannerTitle={(
-          <h3 className="text-2xl font-bold tracking-tight text-primary-white">👋 Hello</h3>
-        )}
-        bannerDescription={
-          <Title 
-            title="Insights about coding, entrepreneurship, tools, and the in-betweens"
-            className={cn(titleVariants({
-              variant: "neutral",
-              size: "lg",
-            }))}
-          />
-        }
-      />
-      <Header
-        className="flex flex-col justify-center items-center gap-6"
-        headerTitle={
-          <Title 
-            title="All Blog Articles"
-            className={cn(titleVariants({
-              variant: "secondary",
-              size: "lg",
-              className: "capitalize"
-            }))}
-          />
-        }
-        headerDescription={
-          <Description
-            description="What's on our mind currently"
-            className={cn(descriptionVariants({
-              variant: "neutral",
-              size: "lg"
-            }))}
-          />
-        }
-      />
+      <Banner className={cn(bannerVariants({  variant: "center", size: "half", className: "flex-col bg-primary rounded-[6px]"}))}>
+      <BannerHeader className="flex flex-col justify-start items-start gap-10 max-w-[350px] sm:max-w-[600px] pt-12">
+        <h3 className="text-2xl font-bold tracking-tight text-primary-white">👋 Hello</h3>
+          <Header
+              className="flex flex-col justify-start items-start gap-6"
+              headerTitle={
+                <Title 
+                  title="All Blog Articles"
+                  className={cn(titleVariants({
+                    variant: "neutral",
+                    size: "xl",
+                    className: "capitalize"
+                  }))}
+                />
+              }
+              headerDescription={
+                <Description
+                  description="Insights about coding, entrepreneurship, tools, and the in-betweens"
+                  className={cn(descriptionVariants({
+                    variant: "neutral",
+                    size: "default"
+                  }))}
+                />
+              }
+            />
+        </BannerHeader> 
+      </ Banner>
       <section className="grid grid-cols-[1fr] grid-auto-rows gap-4 justify-center">
         {blogs.map((blog, index) => (
           <div key={index} className="bg-primary-white mx-auto max-w-2xl lg:mx-0 lg:flex lg:max-w-none">
@@ -75,15 +62,15 @@ export default async function BlogPage() {
               <div className="flex justify-start items-center gap-2">
                 {blog?.tags?.map(({ title, variant}) => (
                   <Tag 
-                    key={title} 
-                    tag={'# ' + title}
-                    className={cn(tagVariants({ variant, size: 'lg' }))}
+                  key={title} 
+                  tag={'# ' + title}
+                  className={cn(tagVariants({ variant, size: 'lg' }))}
                   /> 
                 ))}
                 {blog?.date && (
                   <Tag
-                    tag={format('dd-MMMM-yyyy', parseISO(blog.date)).split('-').join(' ')}
-                    className={cn(tagVariants({ variant: 'secondaryOutlined', size: 'lg' }))}
+                  tag={format('dd-MMMM-yyyy', parseISO(blog.date)).split('-').join(' ')}
+                  className={cn(tagVariants({ variant: 'secondaryOutlined', size: 'lg' }))}
                   />
                 )}
               </div>
@@ -101,7 +88,7 @@ export default async function BlogPage() {
                   style={{ width: "100%", height: "100%" }}
                   objectFit="cover"
                   altText={blog?.title || ''}
-                />
+                  />
               </Link>
             </div>
           </div>
