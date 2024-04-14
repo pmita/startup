e0589d5111dc41e43479948a18b5b4795bcc3217
pmita <panos.mita2@outlook.com>
2024-04-14 18:48:30 +0100

    style(chapters-list): raise html semantics up a level

--- app/(content)/courses/[slug]/[...id]/_components/chapters-list.tsx
+++ app/(content)/courses/[slug]/[...id]/_components/chapters-list.tsx
@@ -13,8 +13,11 @@ type ChapterListProps = {
 }
 
 export default async function ChaptersList({ chapters }: ChapterListProps) {
+
+  if (!chapters) return null;
+
   return (
-    <aside className="flex-[1_1_300px] self-stretch flex flex-col flex-start items-between gap-2 order-2 lg:order-1 min-h-[90vh] overflow-scroll">
+    <>
       {chapters.map((chapter) => (
         <Link href={`/courses/${chapter.slugAsParams}`} key={chapter.weight} className="hover:text-primary-green">
           <div className="flex justify-between items-center p-2 min-h-[48px]">
@@ -42,6 +45,6 @@ export default async function ChaptersList({ chapters }: ChapterListProps) {
           </div>
         </Link>
       ))}
-    </aside>
+    </>
   )
 }
--- app/(content)/courses/[slug]/[...id]/layout.tsx
+++ app/(content)/courses/[slug]/[...id]/layout.tsx
@@ -21,7 +21,9 @@ export default function CourseChapterLayout({ children, params }: CourseChapterL
 
   return (
     <section className="flex flex-wrap flex-row justify-center items-stretch">
-      <ChaptersList chapters={chapters} />
+      <aside className="flex-[1_1_300px] self-stretch flex flex-col flex-start items-between gap-2 order-2 lg:order-1 min-h-[90vh] overflow-scroll">
+        <ChaptersList chapters={chapters} />
+      </aside>
       <section className="flex-[4_1_670px] self-stretch w-full order-1 lg:order-2">
         {children}
       </section>
